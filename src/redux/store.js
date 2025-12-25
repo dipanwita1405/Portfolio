69ca7494b4fc5bd22facb64d9d0388d807142153
dipanwita1405 <dipanwita3018@gmail.com>
2025-12-26 01:54:50 +0530

    Initial portfolio commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,35 @@
-import { createStore, applyMiddleware } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension'
-import thunk from 'redux-thunk'
-import rootReducer from './reducers'
+// import { createStore, applyMiddleware } from 'redux'
+// import { composeWithDevTools } from 'redux-devtools-extension'
+// import thunk from 'redux-thunk'
+// import rootReducer from './reducers'
 
-const initialState = {}
+// const initialState = {}
 
-const middleware = [thunk]
+// const middleware = [thunk]
+
+// const store = createStore(
+//   rootReducer,
+//   initialState,
+//   composeWithDevTools(applyMiddleware(...middleware))
+// )
+
+// export default store
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
+
+const initialState = {};
+
+const middleware = [thunk];
+
+// Use Redux DevTools if available (browser extension), else fallback
+const composeEnhancers =
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   rootReducer,
   initialState,
-  composeWithDevTools(applyMiddleware(...middleware))
-)
+  composeEnhancers(applyMiddleware(...middleware))
+);
 
-export default store
+export default store;
